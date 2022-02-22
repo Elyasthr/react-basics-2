69604dd366c41937542deb1bfecfd9471846eb9b
Elyas <touahria.elyas@outlook.com>
2022-02-22 19:09:51 +0100

    search  movies api

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <nav>
+        <ul>
+          <NavLink to="/" className={(nav)=>(nav.isActive ? "nav-active" : "")}><li>Home</li></NavLink>
+          <NavLink to="/coup-de-coeur" className={(nav)=>(nav.isActive ? "nav-active" : "")}><li>Coup de coeur</li></NavLink>
+        </ul>
+      </nav>
+      <h1>React movies</h1>
+    </div>
+  );
+};
+
+export default Header;
